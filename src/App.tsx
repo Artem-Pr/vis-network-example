484f10c0ef76720029fd200643e43f80ddde861b
Artem Pryadkin <pryadkin1987@gmail.com>
2021-06-10 20:29:46 +0300

    simple example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Network } from 'vis-network/standalone/umd/vis-network.min'
 
 function App() {
-  return <div className="App"></div>
+  const networkRef = useRef(null)
+
+  useEffect(() => {
+    const nodes = [
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' }
+    ]
+
+    const edges = [
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 },
+      { from: 3, to: 3 }
+    ]
+
+    const data = {
+      nodes: nodes,
+      edges: edges
+    }
+    const options = {}
+
+    const container: HTMLElement | null = networkRef.current
+    container && new Network(container, data, options)
+  }, [])
+
+  return <div className="App" ref={networkRef} />
 }
 
 export default App
